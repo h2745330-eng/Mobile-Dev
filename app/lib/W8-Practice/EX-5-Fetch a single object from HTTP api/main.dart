fe49cb50f72e58e23c2130a6b817f065d87de851
Heng <h2745330@gmail.com>
2026-06-29 04:39:36 +0700

    Week 8 Practice

--- app/lib/W8-Practice/EX-5-Fetch a single object from HTTP api/main.dart
+++ app/lib/W8-Practice/EX-5-Fetch a single object from HTTP api/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
 void main() async {
-  Uri url = Uri.parse('https://fooapi.com/api/products');
+  Uri url = Uri.parse('https://jsonplaceholder.typicode.com/users/1');
 
   // 1- Get the request response
   Response response = await http.get(url);
@@ -16,35 +16,76 @@ void main() async {
 
   // 3- Parse the response
   Map<String, dynamic> json = jsonDecode(response.body);
-  List<dynamic> jsonItems = json["data"];
-
-  Map<String, dynamic> jsonItem = jsonItems[0];
-
-  Product product = Product.fromJson(jsonItem);
-  print(product);
+  User user = User.fromJson(json);
+  print(user);
 }
 
-class Product {
-  final String title;
-  final double price;
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String phone;
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.phone,
+  });
 
-  Product({required this.title, required this.price});
-
-  static Product fromJson(Map<String, dynamic> json) {
-    const String titleKey = 'title';
-    const String priceKey = 'price';
-
-    assert(json[titleKey] is String);
-    assert(json[priceKey] is double);
-
-    String title = json[titleKey];
-    double price = json[priceKey];
- 
-    return Product(title: title, price: price);
+  static User fromJson(Map<String, dynamic> json) {
+    const String idKey = 'id';
+    const String nameKey = 'name';
+    const String usernameKey = 'username';
+    const String emailKey = 'email';
+    const String phoneKey = 'phone';
+    assert(json[idKey] is String);
+    assert(json[nameKey] is String);
+    assert(json[usernameKey] is String);
+    assert(json[emailKey] is String);
+    assert(json[phoneKey] is String);
+    int id = json[idKey];
+    String name = json[nameKey];
+    String username = json[usernameKey];
+    String email = json[emailKey];
+    String phone = json[phoneKey];
+    return User(
+      id: id,
+      name: name,
+      username: username,
+      email: email,
+      phone: phone,
+    );
   }
 
   @override
   String toString() {
-    return "Product $title - price= $price";
+    return "User id is $id\nName is $name\nUsername is $username\nEmail is $email\nPhone is $phone";
   }
 }
+
+// class Product {
+//   final String title;
+//   final double price;
+
+//   Product({required this.title, required this.price});
+
+//   static Product fromJson(Map<String, dynamic> json) {
+//     const String titleKey = 'title';
+//     const String priceKey = 'price';
+
+//     assert(json[titleKey] is String);
+//     assert(json[priceKey] is double);
+
+//     String title = json[titleKey];
+//     double price = json[priceKey];
+
+//     return Product(title: title, price: price);
+//   }
+
+//   @override
+//   String toString() {
+//     return "Product $title - price= $price";
+//   }
+// }
